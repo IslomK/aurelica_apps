f0de25d6fab6c2105add60bf59a0e24a03db70da
islom <i.islam.karimov@gmail.com>
2021-05-06 15:43:33 +0500

    перевел секрет кей в env

--- aurelica_apps/settings.py
+++ aurelica_apps/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+!_rj50k49z%97#z@u+p*n3%_4uizzu*r7$=#t5=zd@52r_s)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
